6f359da2e963dca1612ef8fa5815d19403e4233a
Elattari Mohssine <43855033+MohssineElAttari@users.noreply.github.com>
2022-02-10 11:50:48 +0100

    update in group service

--- src/main/java/com/example/bamcoreport/service/GroupService.java
+++ src/main/java/com/example/bamcoreport/service/GroupService.java
@@ -3,6 +3,7 @@ package com.example.bamcoreport.service;
 import com.example.bamcoreport.model.entity.Group;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface GroupService {
 
@@ -14,5 +15,5 @@ public interface GroupService {
 
     String deleteGroup(long id);
 
-    Group getGroupById(long id);
+    Optional<Group> getGroupById(long id);
 }
